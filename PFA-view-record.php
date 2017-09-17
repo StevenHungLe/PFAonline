e9130e754ad6a1020a75ef5c0bb7f54a4fbb4958
Steven Le <steventhecoder@gmail.com>
2017-09-17 17:02:40 -0500

    Added filter and aesthetic features

--- PFA-view-record.php
+++ PFA-view-record.php
@@ -1,34 +1,57 @@
 <!DOCTYPE html>
+<?php
+	$conn = new mysqli("localhost","stevepf6_PFA","987654321","stevepf6_PFA");
+	
+	if(isset($_POST['submit']) && $_POST['from'])
+	{
+		$to_date =  $_POST['to']?$_POST['to']:date('Y-m-d');
+		$filter = " WHERE date >= \"{$_POST['from']}\" AND date <= \"{$to_date}\"";
+	}
+	else
+		$filter="";
+	
+	$result = $conn->query("select * from income".$filter);
+	while ($row = $result->fetch_row())
+	{
+		$rows[] = $row;
+	}
+	
+	$total_cash = $conn->query("select SUM(amount_cash) as total_cash from income".$filter);
+	$total_cash = $total_cash->fetch_row();
+	$total_credit = $conn->query("select SUM(amount_credit) as total_credit from income".$filter);
+	$total_credit = $total_credit->fetch_row();
+	$total = $total_cash[0]+ $total_credit[0];
+	$conn->close();
+?>
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" >
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-		<style>
-		table
-		{
-			background-color:#ccffcc;
-		}
-		th{
-			text-align:center;
-		}
-		td{
-			padding: 5px;
-		}
-		</style>
+		<link rel="stylesheet" href="PFAstyle.css">
 	</head>
 	<body>
+	<table class='table-bordered'>
+		<tr>
+			<th>Date</th>
+			<th>Cash</th>
+			<th>Credit</th>
+			<th>Total</th>
+		</tr>
 	<?php
-	$conn = new mysqli("localhost","stevepf6_PFA","987654321","stevepf6_PFA");
-	$result = $conn->query("select * from income;");
-	echo "<table class='table-bordered'><tr><th>Amount</th><th>Date</th><th>Type</th></tr>";
-	while($row = $result->fetch_row())
+	foreach($rows as $row)
 	{
-	   printf("<tr><td>$ %.2f</td><td>%s</td><td>%s</td></tr>",$row[0],$row[1],$row[2]);
+	   printf("<tr><td>%s</td><td>%.2f</td><td>%.2f</td><td>%.2f</td></tr>",$row[0],$row[1],$row[2],$row[1]+$row[2]);
 	}
-	echo "</table><br>";
-	echo "<a href='PFAonline.php'>Go Back</a>";
-	$conn->close();
+	echo "<tr><th>Total</th><td>$ {$total_cash[0]}</td><td>$ {$total_credit[0]}</td><td>$ {$total}</td></tr>";
 	?>
+	</table><br>
+	<h4>Filter:</h4>
+	<form action="/PFA-view-record.php" method="post">
+	From date:<br><input type="date" name="from"><br>
+	To date:<br><input type="date" name="to"><br><br>
+	<input type="submit" name="submit" value="Filter Result">
+	</form>
+	<a href='PFAonline.php'>Go Back</a>
 	</body>
 </html>
 
--- PFAonline.php
+++ PFAonline.php
@@ -3,36 +3,37 @@
 <head>
     <title>Personal Financial Assistant Online</title>
     <meta name="viewport" content="width=device_width, initial-scale=1">
-    <link rel="stylesheet" href="http://www.w3schools.com/w3css/4/w3mobile.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
 </head>
 <body>
 <?php
     if ($_POST['submit'])
     {
         $conn = new mysqli('localhost','stevepf6_PFA','987654321','stevepf6_PFA');
-       $date = $_POST['date']?$_POST['date']:date('Y-m-d');
-       $stmt = $conn->prepare ("INSERT INTO income VALUES(?,?,?)");
-        $stmt->bind_param('dss',doubleval($_POST['amount']),$date,$_POST['type']);
+		$date = $_POST['date']?$_POST['date']:date('Y-m-d');
+		$stmt = $conn->prepare ("INSERT INTO income VALUES(?,?,?)");
+		$stmt->bind_param('sdd',$date, doubleval($_POST['amount_cash']),doubleval($_POST['amount_credit']));
 	if ( $stmt->execute())
 	{
-		 echo "<script>alert('record of income successful!')</script>";
+		 echo "<h3 style='color:red'>Record of income successful!</h3><br>";
 	}
         $conn->close();
     }				
 ?>
-<div class="w3-container">
-<h2>Income Record Form</h2>
+<div class="container-fluid" >
+<div class="col-sm-4 center-block" style="border:1px solid #cecece; float:none; background: #ccffcc">
+<h3>Income Record Form</h3>
 <form action="/PFAonline.php" method="post">
-    Amount:<br>
-    <input type="text" name="amount" autofocus="autofocus"><br>
-    Type:<br>
-    <input type="radio" name="type" value="credit card" checked> Credit Card<br>
-    <input type="radio" name="type" value="cash"> Cash<br>
-    Date:(today by default)<br>
+    Cash:<br>
+    <input type="text" name="amount_cash" autofocus="autofocus"><br>
+    Credit:<br>
+    <input type="text" name="amount_credit" autofocus="autofocus"><br>
+    Date:<br>
     <input type="date" name="date"><br><br>
     <input type="submit" name="submit"><br><br>
     <button type="submit" name="view record" formaction="PFA-view-record.php">View Record</button>
 </form>
+</div>
 </div>
 </body>
 </html>
